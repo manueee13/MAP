d77a6f0e50a7f27c4cd450268826514266bff49b
Emanuele Tamborrino <temanuele27@gmail.com>
2025-01-07 15:47:31 +0100

    Rimossi metodi non utilizzati

--- systembreach/src/main/java/com/manueee/systembreach/util/sessions/SessionUtils.java
+++ systembreach/src/main/java/com/manueee/systembreach/util/sessions/SessionUtils.java
@@ -23,44 +23,6 @@ public class SessionUtils {
             return new FileSystem();
         }
     }
-    
-    private static FileSystem parseJsonToFileSystem(JsonObject json) {
-        FileSystem fs = new FileSystem();
-        if (json == null || !json.has("root")) {
-            return fs;
-        }
-    
-        JsonObject rootNode = json.getAsJsonObject("root");
-        if (rootNode != null && rootNode.has("children")) {
-            parseDirectory(rootNode, fs);
-        }
-        return fs;
-    }
-    
-    private static void parseDirectory(JsonObject dirJson, FileSystem fs) {
-        if (dirJson == null || !dirJson.has("type") || !dirJson.has("name")) {
-            return;
-        }
-    
-        String type = dirJson.get("type").getAsString();
-        String name = dirJson.get("name").getAsString();
-        
-        if ("directory".equals(type) && dirJson.has("children")) {
-            JsonObject children = dirJson.getAsJsonObject("children");
-            for (String childName : children.keySet()) {
-                JsonObject child = children.getAsJsonObject(childName);
-                if (child.has("type")) {
-                    if ("file".equals(child.get("type").getAsString())) {
-                        String content = child.has("content") ? 
-                            child.get("content").getAsString() : "";
-                        fs.createFile(name + "/" + childName, content);
-                    } else {
-                        parseDirectory(child, fs);
-                    }
-                }
-            }
-        }
-    }
 
     public static void loadSession(File file) {
         // TODO
